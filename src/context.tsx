1d7c5f00bdc72b98595947ffeea997aec53459d4
Joe Milius <joemilius@gmail.com>
2024-08-14 19:24:30 -0600

    context added for state of user

---
+++ src/context.tsx
@@ -0,0 +1,42 @@
+import React, { createContext, PropsWithChildren, useState } from 'react'
+
+export interface IUser {
+    id: number,
+    username: string,
+    email: string,
+    first_name: string,
+    last_name: string,
+    logins: number,
+    password: string
+}
+
+const useValue = () => {
+    const[user, setUser] = useState<IUser>()
+
+    return{
+        user,
+        setUser
+    }
+}
+
+export const StateContext = createContext({} as ReturnType<typeof useValue>)
+
+export const StateContextProvider: React.FC<PropsWithChildren> = (props) => {
+    return(
+                    <StateContext.Provider value={useValue()}>
+                        {props.children}
+                    </StateContext.Provider>
+            )
+}
+
+// export function StateProvider ({ children }) {
+//     const[user, setUser] = useState(null)
+
+//     return(
+//         <div>
+//             <StateContext.Provider value={{user , setUser}}>
+//                 {children}
+//             </StateContext.Provider>
+//         </div>
+//     )
+// }
