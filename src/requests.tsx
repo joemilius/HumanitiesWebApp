f086a6cf4b64d8ab5c8819598760595774d3e527
Joe Milius <joemilius@gmail.com>
2024-08-19 17:03:04 -0600

    reusable patchRequest function created

--- src/requests.tsx
+++ src/requests.tsx
@@ -13,4 +13,50 @@ const getRequest = async (url: string) => {
     }
 }
 
-export {getRequest}
+const postRequest = async (url:string, body:object) => {
+    try{
+        const configObj = {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(body)
+        }
+        const res = await fetch(url, configObj)
+
+        const data = await res.json()
+
+        if(!res.ok){
+            throw new Error(data.error)
+        }
+        return data
+    }catch(error){
+        return error
+    }
+
+}
+
+const patchRequest = async (url:string, body:object) => {
+    try{
+        const configObj = {
+            method: "PATCH",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(body)
+        }
+        const res = await fetch(url, configObj)
+
+        const data = await res.json()
+
+        if(!res.ok){
+            throw new Error(data.error)
+        }
+        return data
+    }catch(error){
+        return error
+    }
+
+}
+
+export {getRequest, postRequest, patchRequest}
