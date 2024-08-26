7ad414e46d0871616a294857a59bbdbd25d8d172
Joe Milius <joemilius@gmail.com>
2024-08-26 12:46:27 -0600

    signup form created and controlled with state

--- src/pages/Signup.tsx
+++ src/pages/Signup.tsx
@@ -1,10 +1,32 @@
+import React, {useState, useContext} from 'react'
+import { StateContext } from '../context'
 
 
 function Signup(){
+    const {currentUser, setCurrentUser} = useContext(StateContext)
+    const [signupData, setSignupData] = useState({
+        username: '',
+        email: '',
+        first_name: '',
+        last_name: ''
+    })
+
+    function handleSignupChange(e: React.SyntheticEvent<HTMLInputElement>){
+        setSignupData({
+            ...signupData, [e.currentTarget.name]: e.currentTarget.value
+        })
+    }
 
 
     return(
         <>
+        <form>
+            <input type='text' name='first_name' onChange={handleSignupChange} placeholder='Enter first name here'></input>
+            <input type='text' name='last_name' onChange={handleSignupChange} placeholder='Enter last name here'></input>
+            <input type='text' name='email' onChange={handleSignupChange} placeholder='Enter email here'></input>
+            <input type='text' name='username' onChange={handleSignupChange} placeholder='Enter username here'></input>
+            <input type='submit'></input>
+        </form>
         </>
     )
 }
