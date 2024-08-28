4bb1bd3903ba16f7e43d2800dc1c2f8c29c80f23
Joe Milius <joemilius@gmail.com>
2024-08-28 07:24:27 -0600

    formik with yup initialized for signup

--- src/pages/Signup.tsx
+++ src/pages/Signup.tsx
@@ -1,5 +1,7 @@
 import React, {useState, useContext} from 'react'
 import { StateContext } from '../context'
+import * as yup from 'yup'
+import {useFormik} from 'formik'
 
 
 function Signup(){
@@ -10,7 +12,59 @@ function Signup(){
         first_name: '',
         last_name: ''
     })
+    const signupConfig = {
+        initialValue: {
+        username: '',
+        email: '',
+        first_name: '',
+        last_name: '',
+        password: ''
+        },
 
+        fields: [
+            {
+                label: 'Username',
+                name: 'username',
+                type: 'text'
+            },
+            {
+                label: 'Email',
+                name: 'email',
+                type: 'text'
+            },
+            {
+                label: 'First Name',
+                name: 'first_name',
+                type: 'text'
+            },
+            {
+                label: 'Last Name',
+                name: 'last_name',
+                type: 'text'
+            },
+            {
+                label: 'Password',
+                name: 'password',
+                type: 'text'
+            },
+        ],
+        formSchema: yup.object().shape({
+            username: yup.string().required('*required'),
+            email: yup.string().required('*required'),
+            first_name: yup.string().required('*required'),
+            last_name: yup.string().required('*required'),
+            password: yup.string().required('*required')
+        })
+        
+    }
+
+    const formik = useFormik({
+        initialValues: signupConfig.initialValue,
+        validationSchema: signupConfig.formSchema,
+        onSubmit: (values, {resetForm}) => {
+            
+        }
+    })
     function handleSignupChange(e: React.SyntheticEvent<HTMLInputElement>){
         setSignupData({
             ...signupData, [e.currentTarget.name]: e.currentTarget.value
