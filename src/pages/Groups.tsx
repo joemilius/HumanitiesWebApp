a6fe86a46b544dd463dc6895346befab3bda5369
Joe Milius <joemilius@gmail.com>
2024-09-07 21:32:17 -0600

    added on click for selecting a particular group to be chosen and set up state in context for use when changing to a groups page

--- src/pages/Groups.tsx
+++ src/pages/Groups.tsx
@@ -1,14 +1,20 @@
 import React, {useContext} from 'react'
-import { StateContext } from '../context'
+import { IGroup, StateContext } from '../context'
 
 function Groups(){
-    const {currentUser} = useContext(StateContext)
+    const {currentUser, setSelectedGroup} = useContext(StateContext)
+
+    function handleGroupClick(group:IGroup){
+        setSelectedGroup(group)
+    }
     return(
         <>
         <h3>My Groups</h3>
             <ul>
                 {currentUser?.groups.map(group => (
-                    <li>{group['group_name']}</li>
+                    <li>{group['group_name']}
+                    <button onClick={() => handleGroupClick(group)}>View Group</button>
+                    </li>
                 ))}
             </ul>
             <h3>My Invitations</h3>
