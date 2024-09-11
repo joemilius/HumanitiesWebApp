3351851e685475bcaff4ddd03478a3530fdba1ce
Joe Milius <joemilius@gmail.com>
2024-09-10 18:57:32 -0600

    context used to grab group information and map of memberships to show members in the group

--- src/pages/OneGroup.tsx
+++ src/pages/OneGroup.tsx
@@ -1,8 +1,21 @@
-import React from 'react'
+import React, {useContext} from 'react'
+import { StateContext } from '../context'
+
+
 
 function OneGroup(){
+    const { selectedGroup } = useContext(StateContext)
+
+    
     return(
-        <></>
+        <>
+        <h2>{selectedGroup?.group_name}</h2>
+        <ul>
+            {selectedGroup?.memberships.map((membership) => {
+                return <li>{membership['user']['username']}</li>
+            })}
+        </ul>
+        </>
     )
 }
 
