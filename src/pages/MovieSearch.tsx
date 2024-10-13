bcd4706f57d6cafaa0d0355fdb2cf77b92fcf841
Joe Milius <joemilius@gmail.com>
2024-10-12 20:10:01 -0600

    state and function to handle media search created

--- src/pages/MovieSearch.tsx
+++ src/pages/MovieSearch.tsx
@@ -4,7 +4,7 @@ function MovieSearch(){
     return(
         <div>
             <form>
-                <input/>
+                <input type='text'/>
                 <button type='submit'>Search Movies</button>
             </form>
         </div>
